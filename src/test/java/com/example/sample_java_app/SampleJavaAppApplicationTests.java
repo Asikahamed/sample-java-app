a94193d23737464e37f009c031b045ceb6e4a509
Asikahamed <asikahamed10@yahoo.com>
2026-06-22 15:53:52 +0530

    testing app

---
+++ src/test/java/com/example/sample_java_app/SampleJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample_java_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
